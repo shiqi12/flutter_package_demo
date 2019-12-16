98a476d2f91fa931aa83d2e11006e143141140d6
Shiqi Zhou <Shiqi.Zhou@partner.bmw.com>
2019-12-16 17:53:16 +0800

    delete unuseful file

--- flutter_package_demo/lib/flutter_package_demo.dart
+++
@@ -1,6 +0,0 @@
-library flutter_package_demo;
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-part "src/custom_widget.dart";
--- flutter_package_demo/test/calculator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_package_demo/flutter_package_demo.dart';
-
-void main() {
-  test('adds one to input values', () {
-  });
-}
